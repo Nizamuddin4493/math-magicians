34bd46b71194c04fe4a7583ed7b9c0b3ffd37ace
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-03-30 15:23:35 +0430

    Math magicians: full website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { PureComponent } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import CalDesign from './components/Calculator';
+import Quote from './components/Quote';
+import Home from './components/Home';
+import Headers from './components/Header';
+import Footer from './components/Footer';
 
 class App extends PureComponent {
   render() {
     return (
-      <CalDesign />
+      <>
+        <Headers />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calDesign" element={<CalDesign />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+        <Footer />
+      </>
     );
   }
 }
